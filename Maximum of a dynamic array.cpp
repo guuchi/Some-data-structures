b7674d0616bf668598fed8900292f15e7f3f28ab
guuchi <38071857+guuchi@users.noreply.github.com>
2018-11-18 16:23:01 +0100

    Create Maximum of a dynamic array.cpp

---
+++ Maximum of a dynamic array.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int PROFONDEUR = 17,premier= pow(2, PROFONDEUR);
+
+int maximal[premier*2];
+
+void update(int node, int value ) 
+{
+   maximal[node] = value;
+   for (int noeudCourant = node/2; noeudCourant > 0; noeudCourant /= 2) 
+      maximal[noeudCourant] = max(maximal[noeudCourant*2], maximal[noeudCourant*2+1]);
+}
+
+int main()
+{
+   int nbQuestions, hauteurArbre;
+   cin >> hauteurArbre >> nbQuestions;
+   
+   for (int idRequete = 0; idRequete < nbQuestions; idRequete++)
+   {
+      int idModif, valeurModif;
+      cin >> idModif >> valeurModif;
+      update(idModif+premier, valeurModif);
+      cout << maximal[1] << endl;
+   }
+}
